88838e91321c598513bfc66c38fc86bcc2d3dcf9
arturhalaczkiewicz <arturhalaczkiewicz@gmail.com>
2018-09-25 20:56:01 +0200

    Initial commit

---
+++ UnitOpony.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+
+#ifndef UnitOponyH
+#define UnitOponyH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.Types.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Edit.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TFlowLayout *FlowLayout1;
+	TLabel *Label1;
+	TFlowLayoutBreak *FlowLayoutBreak1;
+	TComboBox *ComboBoxSN;
+	TLabel *Label2;
+	TComboBox *ComboBoxAR;
+	TLabel *Label3;
+	TComboBox *ComboBoxDR;
+	TFlowLayoutBreak *FlowLayoutBreak2;
+	TLabel *Label4;
+	TEdit *Edit1;
+	TFlowLayoutBreak *FlowLayoutBreak3;
+	TLabel *Label5;
+	TLabel *LabelSrednicaKola;
+	TFlowLayoutBreak *FlowLayoutBreak4;
+	TLabel *Label6;
+	TLabel *LabelObwodKola;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall ComboBoxSNChange(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
